7f8a562e4d3609fe011cede0e8e457332516da38
Levente Antal <antal.levente@minic.ro>
2013-03-06 17:31:21 +0200

    added install.sql sample

---
+++ install.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `PREFIX_minicskeleton` (
+  `id_skeleton` int(10) unsigned NOT NULL,
+  `text` text,
+  PRIMARY KEY (`id_tile`)
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
+
+INSERT INTO `PREFIX_minicskeleton` VALUES (1, 'Some text');
