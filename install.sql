5731f049ec4c57242e3b0ed9a063507855ec8209
Antal Levente <antal.levente@minic.ro>
2013-03-14 10:35:59 +0200

    fixed typo

--- install.sql
+++ install.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `PREFIX_minicskeleton` (
   `id_skeleton` int(10) unsigned NOT NULL,
   `text` text,
-  PRIMARY KEY (`id_tile`)
+  PRIMARY KEY (`id_skeleton`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
 
 INSERT INTO `PREFIX_minicskeleton` VALUES (1, 'Some text');
